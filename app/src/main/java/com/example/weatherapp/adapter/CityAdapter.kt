b5fbc361668f582eb5e5ebbce503e48f3c259ad0
chepugash <chepugash@yahoo.com>
2023-03-13 02:45:31 +0300

    nearest cities added

--- app/src/main/java/com/example/weatherapp/adapter/CityAdapter.kt
+++ app/src/main/java/com/example/weatherapp/adapter/CityAdapter.kt
@@ -2,6 +2,8 @@ package com.example.weatherapp.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.data.response.City
 import com.example.weatherapp.databinding.ItemCityBinding
@@ -9,7 +11,17 @@ import com.example.weatherapp.databinding.ItemCityBinding
 class CityAdapter(
     private val list: List<City>,
     private val action: (Int) -> Unit
-) : RecyclerView.Adapter<CityItem>() {
+) : ListAdapter<City, RecyclerView.ViewHolder>(object: DiffUtil.ItemCallback<City>() {
+    override fun areItemsTheSame(
+        oldItem: City,
+        newItem: City
+    ): Boolean = oldItem.id == newItem.id
+
+    override fun areContentsTheSame(
+        oldItem: City,
+        newItem: City
+    ): Boolean = oldItem == newItem
+}) {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -24,10 +36,10 @@ class CityAdapter(
     )
 
     override fun onBindViewHolder(
-        holder: CityItem,
+        holder: RecyclerView.ViewHolder,
         position: Int
     ) {
-        holder.onBind(list[position])
+        (holder as CityItem).onBind(list[position])
     }
 
     override fun getItemCount(): Int = list.size
--- app/src/main/java/com/example/weatherapp/ui/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/MainFragment.kt
@@ -1,9 +1,13 @@
 package com.example.weatherapp.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.Space
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -15,6 +19,8 @@ import com.example.weatherapp.data.response.City
 import com.example.weatherapp.databinding.FragmentMainBinding
 import com.example.weatherapp.utils.hideKeyboard
 import com.example.weatherapp.utils.showSnackbar
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.launch
 
 private const val DEFAULT_LAT = 49.0
@@ -25,12 +31,30 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     private var binding: FragmentMainBinding? = null
     private var adapter: CityAdapter? = null
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
     private val api = DataContainer.weatherApi
+
+    private var userLat = DEFAULT_LAT
+    private var userLon = DEFAULT_LON
+
+    private val requestPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            loadNearestCities()
+            activity?.findViewById<View>(android.R.id.content)
+                ?.showSnackbar("Granted")
+        } else {
+            createRecyclerView()
+            activity?.findViewById<View>(android.R.id.content)
+                ?.showSnackbar("Not granted")
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMainBinding.bind(view)
-
         binding?.run {
             btnFind.setOnClickListener {
                 onLoadClick()
@@ -41,25 +65,46 @@ class MainFragment : Fragment(R.layout.fragment_main) {
                 }
                 true
             }
-            loadCityRV()
+        }
+        loadNearestCities()
+    }
+
+    private fun loadNearestCities() {
+        if (context?.let {
+            ActivityCompat.checkSelfPermission(
+                it,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            } == PackageManager.PERMISSION_DENIED) {
+            requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        } else {
+            mFusedLocationClient = LocationServices
+                .getFusedLocationProviderClient(requireActivity())
+            mFusedLocationClient.lastLocation.addOnSuccessListener { loc: Location? ->
+                if (loc != null) {
+                    userLat = loc.latitude
+                    userLon = loc.longitude
+                    activity?.findViewById<View>(android.R.id.content)
+                        ?.showSnackbar("Cities founded")
+                    createRecyclerView()
+                } else {
+                    activity?.findViewById<View>(android.R.id.content)
+                        ?.showSnackbar("Not found")
+                }
+            }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
-    }
-
-    private fun loadCityRV() {
+    private fun createRecyclerView() {
+        val itemDecoration = SpaceItemDecorator(
+            this@MainFragment,
+            16.0f
+        )
         lifecycleScope.launch {
             try {
                 showLoading(true)
-                val itemDecoration = SpaceItemDecorator(
-                    this@MainFragment,
-                    16.0f
-                )
                 adapter = CityAdapter(
-                    api.getCities(DEFAULT_LAT, DEFAULT_LON, CNT).list
+                    api.getCities(userLat, userLon, CNT).list
                 ) {
                     parentFragmentManager.beginTransaction()
                         .replace(R.id.container, DetailFragment.newInstance(it))
